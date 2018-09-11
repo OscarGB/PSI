393e8895af424691af0234842c45bb6db12f4689
OscarGB <oscar.gomez.borz@gmail.com>
2018-09-11 20:05:16 +0200

    hail and zip

--- P1/hail.py
+++ P1/hail.py
@@ -25,8 +25,8 @@ def main():
   if len(sys.argv) >= 2:
     name = sys.argv[1]
   else:
-    name = 'Mundo'
-  print 'Hola', name
+    name = 'Welt'
+  print 'Hallo', name
 
 # This is the standard boilerplate that calls the main() function.
 if __name__ == '__main__':
