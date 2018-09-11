f2fdf232c31203818daff07e800e327d51c560c7
SirWednesday <arnaizaitor@gmail.com>
2018-09-11 19:17:36 +0200

    Hola Mundo

--- P1/hello.py
+++ P1/hello.py
@@ -25,8 +25,8 @@ def main():
   if len(sys.argv) >= 2:
     name = sys.argv[1]
   else:
-    name = 'World'
-  print 'Hello', name
+    name = 'Mundo'
+  print 'Hola', name
 
 # This is the standard boilerplate that calls the main() function.
 if __name__ == '__main__':
