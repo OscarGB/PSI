45ee59267251192b879280e1a8705413e7264142
OscarGB <oscar.gomez.borz@gmail.com>
2018-10-02 19:35:37 +0200

    Chapter 5 complete

--- P2/tango_with_django_project/rango/admin.py
+++ P2/tango_with_django_project/rango/admin.py
@@ -2,5 +2,14 @@
 from __future__ import unicode_literals
 
 from django.contrib import admin
+from rango.models import Category, Page
+
+class PageAdmin(admin.ModelAdmin):
+	list_display = ['title','category', 'url']
+	pass
+
+admin.site.register(Category)
+admin.site.register(Page, PageAdmin)
+
 
 # Register your models here.
