d8c22102f279aa4f2d499407d1bd36a16c0dcd41
OscarGB <oscar.gomez.borz@gmail.com>
2018-10-30 18:41:16 +0100

    cosas

--- P3/workflowrepository/data/admin.py
+++ P3/workflowrepository/data/admin.py
@@ -4,16 +4,19 @@ from __future__ import unicode_literals
 from django.contrib import admin
 from data.models import Category, Workflow
 
-class CateegoryAdmin(admin.ModelAdmin):
-	prepopulated_fields = {"slug":("name",)}
+class CategoryAdmin(admin.ModelAdmin):
+	# prepopulated_fields = {'slug':('name',)}
 	list_display = ['name', 'slug']
+	fields = ['name', 'tooltip']
+	# readonly_fields = ['slug']
 
 class WorkflowAdmin(admin.ModelAdmin):
-	prepopulated_fields = {"slug":("name",)}
 	list_display = ['name', 'slug', 'views', 'downloads', 'client_ip', 'created']
+	fields = ['name', 'description', 'versionInit', 'category', 'client_ip', 'keywords', 'json']
+	# readonly_fields = ['slug']
 	pass
 
-admin.site.register(Category, CateegoryAdmin)
+admin.site.register(Category, CategoryAdmin)
 admin.site.register(Workflow, WorkflowAdmin)
 
 # Register your models here.
